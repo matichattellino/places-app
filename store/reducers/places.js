247d32b77fd63fa6f5b0eb8b1bb95710df05c009
matias chattellino <matiaschattellino@gmail.com>
2020-06-23 17:23:48 -0300

    getting user location

--- store/reducers/places.js
+++ store/reducers/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE } from "../actions/places";
+import { ADD_PLACE, SET_PLACES } from "../actions/places";
 
 import Place from '../../models/place';
 
@@ -8,6 +8,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case SET_PLACES:
+            return {
+                places: action.places.map(
+                    pl => new Place(pl.id.toString(), pl.title, pl.imageUri)
+                )
+            };
         case ADD_PLACE:
             const newPlace = new Place(
                 action.placeData.id.toString(), 
